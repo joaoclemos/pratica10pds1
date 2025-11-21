6e53ff844ad5c7c1c139d0f36ded5b3da426ae78
joaoclemos <joaoclemos22@gmail.com>
2025-11-21 18:36:13 -0300

    Q3 nome mudado

--- user.c
+++ user.c
@@ -4,6 +4,34 @@
 #include "user.h"
 #include "address.h"
 
+struct address *create_address(const char *entrada) {
+    struct address *novo_endereco = (struct address*) malloc(sizeof(struct address));
+
+    const char *sep1 = strchr(entrada, '|');
+    const char *sep2 = strchr(sep1 + 1, '|');
+
+    int tam_linha = sep1 - entrada;
+
+    novo_endereco->line = (char*) malloc((tam_linha + 1) * sizeof(char));
+    
+    if (novo_endereco->line != NULL) {
+        strncpy(novo_endereco->line, entrada, tam_linha);
+        novo_endereco->line[tam_linha] = '\0';
+    }
+
+    novo_endereco->number = atoi(sep1 + 1);
+    novo_endereco->zipcode = atoi(sep2 + 1);
+
+    return novo_endereco;
+}
+
+void free_address(struct address *endereco) {
+    if (endereco != NULL) {
+        free(endereco->line);
+        free(endereco);
+    }
+}
+
 struct user *create_user(const char *entrada) {
     struct user *novo_usuario = (struct user*) malloc(sizeof(struct user));
 
